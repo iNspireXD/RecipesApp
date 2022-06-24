d24c5b2bfcdcc87761d90bc4801ca157fc8eb03d
Aswin Karki <aswin.karkii44@gmail.com>
2022-06-24 12:57:00 +0545

    completed categories screen and overview screen

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,53 @@
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { useLayoutEffect } from "react";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+
+const MealsOverviewScreen = ({ route, navigation }) => {
+  const catId = route.params.categoryId;
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
+
+  const renderMealItem = (itemData) => {
+    const item = itemData.item;
+
+    const mealItemsProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    };
+
+    return <MealItem {...mealItemsProps} />;
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+};
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
